f4e364177cc7e264eeba6e340cba0f39d55be446
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-17 18:48:29 +0530

    updated cron jobs

--- backend/cron.js
+++ backend/cron.js
@@ -3,7 +3,7 @@ import https from 'https'
 
 const URL = "https://tonic.saurabhparyani.dev"
 
-const job = new cron.CronJob('*/14 * * * *', function(){
+const job = new cron.CronJob('*/10 * * * *', function(){
     https.get(URL, (res) => {
         if (res.statusCode == 200){
             console.log('Request sent successfully!')
